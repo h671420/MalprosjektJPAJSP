845c7668c11f14d3b66a1c7372cd7b136319ec1c
h671420 <671420@stud.hvl.no>
2024-03-01 12:49:20 +0100

    meh

--- src/main/java/no/hvl/malprosjekt/MalProsjekt.java
+++ src/main/java/no/hvl/malprosjekt/MalProsjekt.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MalProsjekt {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MalProsjekt.class, args);
 	}
 
